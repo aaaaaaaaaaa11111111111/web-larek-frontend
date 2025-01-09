cb57e2263b73cfd904179a13068ef1af2aab63f7
daniil savelev <daniilsav2000@yandex.ru>
2025-01-09 13:33:49 +0400

    lil appdata doc edit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { EventEmitter } from './components/base/events';
 import './scss/styles.scss';
+import { LarekApi } from './components/LarekApi';
+import { API_URL, CDN_URL } from './utils/constants';
 
 const events = new EventEmitter();
+const api = new LarekApi(API_URL, CDN_URL);
+
+api.getProductsList()
+  .then((products) => {
+    console.log('products list:', products)
+  })
+  .catch((error) => {
+    console.log('nasral oshibok', error)
+  });
