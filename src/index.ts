70a4ecc14bd9a6b234972ac0b5e6157aeccf0c10
akiyama <daniilsav2000@yandex.ru>
2025-01-10 02:53:05 +0400

    just added common classes from example

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import { EventEmitter } from './components/base/events';
 import './scss/styles.scss';
+import { EventEmitter } from './components/base/events';
 import { LarekApi } from './components/LarekApi';
 import { API_URL, CDN_URL } from './utils/constants';
 
 const events = new EventEmitter();
 const api = new LarekApi(API_URL, CDN_URL);
 
-api.getProductsList()
-  .then((products) => {
-    console.log('products list:', products)
-  })
-  .catch((error) => {
-    console.log('nasral oshibok', error)
-  });
+api
+	.getProductsList()
+	.then((products) => {
+		console.log('products list:', products);
+	})
+	.catch((error) => {
+		console.log('nasral oshibok', error);
+	});
