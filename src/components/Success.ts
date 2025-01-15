7864ee114702b19dcc2c3fe253a4abf9b8199e77
daniil savelev <daniilsav2000@yandex.ru>
2025-01-15 16:16:25 +0400

    Success added

--- src/components/Success.ts
+++ src/components/Success.ts
@@ -1,3 +1,4 @@
+import { ensureElement } from "../utils/utils";
 import { Component } from "./base/Component";
 
 interface ISuccess {
@@ -9,5 +10,20 @@ interface ISuccessActions {
 }
 
 export class Success extends Component<ISuccess> {
-    protected _total
+    protected _total: HTMLElement;
+    protected _close: HTMLElement;
+
+    constructor(container: HTMLElement, actions: ISuccessActions) {
+        super(container);
+
+        this._total = ensureElement<HTMLElement>('.order-success__description', this.container);
+        this._close = ensureElement<HTMLElement>('.order-success__close', this.container);
+        if(actions?.onClick) {
+            this._close.addEventListener('click', actions.onClick);
+        }
+    }
+
+    set total(total: string) {
+        this._total.textContent = `Списано ${total} синапсов`;
+    }
 }
